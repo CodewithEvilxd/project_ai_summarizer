d3ec6c024ef5d7eeed48ec98c1c47d0d06004375
nishantgauravhere <126612139+CodewithEvilxd@users.noreply.github.com>
2024-04-08 18:59:35 +0530

    commet

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -20,7 +20,7 @@ const Hero = () => {
       </nav>
 
       <h1 className='head_text'>
-        Summarize Articles with <br className='max-md:hidden' />
+        Summarize Articles with EvilXD <br className='max-md:hidden' />
         <span className='orange_gradient '>OpenAI GPT-4</span>
       </h1>
       <h2 className='desc'>
